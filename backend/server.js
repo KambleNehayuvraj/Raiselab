3147085f5e2c70e5a239a07e95e73d01fb2aa346
Neha Kamble <nehakamble7232@gmail.com>
2026-08-02 15:59:06 +0530

    allow admin panel to connect to backend

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,8 @@ app.use(cors({
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://localhost:5174",
-    "http://127.0.0.1:5174"
+    "http://127.0.0.1:5174",
+    "https://raiselab-admin.onrender.com"
   ],
   credentials: true
 }));
